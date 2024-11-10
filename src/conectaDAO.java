575f789ecc186b964f5217ba89dbaca4260ab423
Victor Couto <victorcoutocidade@gmail.com>
2024-11-10 16:03:49 -0300

    Removidos os comentários automaticos so JAVA

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -5,16 +5,6 @@ import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
     
     public Connection connectDB(){
